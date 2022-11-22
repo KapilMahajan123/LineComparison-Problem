704a77fb44e89815b0619b704888b4de5861cc5a
kapilmahajan123 <montyempire123gmail.com>
2022-11-22 13:16:20 +0530

    Added: Comparison between lines

--- src/com/LineComparison/LineComparison.java
+++ src/com/LineComparison/LineComparison.java
@@ -25,16 +25,19 @@ public class LineComparison {
 		double length1;
 		double length2;
 		
-		length1 = Math.sqrt(Math.pow(x2 - x1,2) + Math.pow(y2 - y1,2));
+		length1 = Math.sqrt(Math.pow(x2 - x1,2) + Math.pow(y2 - y1,2)); //Finding lengths
 		System.out.println("Length Of line1: "+length1);
 		length2 = Math.sqrt(Math.pow(x4 - x3,2) + Math.pow(y4 - y3,2));
 		System.out.println("Length Of Line2: "+length2);
 		
 		if(length1 == length2) {
-			System.out.println("Both Length Are Equal");
+			System.out.println("Both Lines Are Equal");
+		}
+		else if(length1 > length2){
+			System.out.println("Length of First Line is Greater");
 		}
 		else {
-			System.out.println("Not Equal");
+			System.out.println("Length of Second Line is Greater");
 		}
 
 	}
