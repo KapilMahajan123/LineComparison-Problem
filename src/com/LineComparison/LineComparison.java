10db3f37045483ee139766df319c6abb4b7f4ac8
kapilmahajan123 <montyempire123gmail.com>
2022-11-22 12:42:54 +0530

    Added: Dispaly the LineComparison Massage

---
+++ src/com/LineComparison/LineComparison.java
@@ -0,0 +1,9 @@
+package com.LineComparison;
+
+public class LineComparison {
+	
+	public static void main(String[] args) {
+		System.out.println("~~WELCOME TO LINE COMPARISON COMPUTATION PROGRAM~~");
+
+	}
+}
