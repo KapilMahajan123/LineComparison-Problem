8d03c023f7c75baabde5bff226ac9fa3f07a158f
KapilMahajan123 <111445779+KapilMahajan123@users.noreply.github.com>
2022-11-24 08:22:12 +0530

    Added: Calculating Length

--- src/com/LineComparison/LineComparison.java
+++ src/com/LineComparison/LineComparison.java
@@ -12,11 +12,13 @@ public class LineComparison {
 	public static void main(String[] args) {
 		System.out.println("~~WELCOME TO LINE COMPARISON COMPUTATION PROGRAM~~");
 		Scanner sc = new Scanner(System.in);// creating object
-		System.out.println("Enter Value X1 X2 Y1 Y2");
-		int x1 = sc.nextInt();
-		int x2 = sc.nextInt();
-		int y1 = sc.nextInt();
-		int y2 = sc.nextInt();
+		System.out.println("Enter Value X1 X2 Y1 Y2");  //Printing enter values
+		
+		//Input integers and calling the nextInt
+		int x1 = scanner.nextInt();
+		int x2 = scanner.nextInt();
+		int y1 = scanner.nextInt();
+		int y2 = scanner.nextInt();
 		double length;
 		
 		length = Math.sqrt(Math.pow(x2 - x1,2) + Math.pow(y2 - y1,2));
