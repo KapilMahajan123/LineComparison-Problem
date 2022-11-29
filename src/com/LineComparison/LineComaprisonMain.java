32d207960e0a8b5532695ce3346f7609aaf378ee
kapilmahajan123 <montyempire123gmail.com>
2022-11-29 17:02:15 +0530

    Added: Comparing Line using Oops Concept

---
+++ src/com/LineComparison/LineComaprisonMain.java
@@ -0,0 +1,16 @@
+package com.LineComparison;
+
+public class LineComaprisonMain {		
+	
+		public static void main(String[] args) {
+			System.out.println("~~WELCOME TO LINE COMPARISON COMPUTATION PROGRAM~~");
+			
+			//Creating object
+			LineComparison linecomparison = new LineComparison();  
+			
+			//Calling method
+			linecomparison.getLenght();
+			linecomparison.compareLines();
+			
+		}
+}
